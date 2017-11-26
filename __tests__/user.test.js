55cf34a2fbdd5584ed19ff35d64432a6df8e11fc
fredriko83 <folep02@student.lnu.se>
2017-11-26 11:27:17 +0100

    setting up jest

---
+++ __tests__/user.test.js
@@ -0,0 +1,30 @@
+const sum = require('../lib/sum');
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test('adds 1 + 2 to equal 4', () => {
+    expect(sum(1, 2)).toBe(4);
+  });
+
+// Dont know if we can do integration tests on CCI?
+
+//const app = require('../index');
+//const request = require('supertest');
+
+// describe('Test the user path', () => {
+//     test('It should response the GET method', () => {
+//         return request(app).get("/user").then(response => {
+//             expect(response.statusCode).toBe(200)
+//         })
+//     });
+// })
+
+// describe('Test the user/fail! path', () => {
+//     test('It should response the GET method', () => {
+//         return request(app).get("/user/fail!").then(response => {
+//             expect(response.statusCode).toBe(200)
+//         })
+//     });
+// })
