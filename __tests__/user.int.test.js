590f4b1640af4396666bc87365638632b590f3cf
Leif Karlsson <leif.karlsson@stavos.ax>
2017-12-15 16:57:03 +0200

    breaks out unit and integration tests in pipeline, creating chaos in its wake

---
+++ __tests__/user.int.test.js
@@ -0,0 +1,18 @@
+const app = require('../index');
+const request = require('supertest');
+
+describe('Test the user path', () => {
+  test('It should response the GET method', () => {
+    return request(app).get('/user').then(response => {
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
+
+// describe('Test the user/fail! path', () => {
+//     test('It should response the GET method', () => {
+//         return request(app).get("/user/fail!").then(response => {
+//             expect(response.statusCode).toBe(200)
+//         })
+//     });
+// })
