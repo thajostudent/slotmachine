146c7fc6dae7cf1a3c9b6920425b97a18e585241
leifkarlsson <lk222rc@student.lnu.se>
2017-12-20 12:57:07 +0200

    testing, testing

--- model/user/controller.js
+++ model/user/controller.js
@@ -1,6 +1,11 @@
 const Controller = require('../../lib/controller');
 const userFacade = require('./facade');
 
-class UserController extends Controller {}
+class UserController extends Controller {
+  // Just testing
+  create(req, res, next) {
+    res.send(req.body).status(200);
+  }
+}
 
 module.exports = new UserController(userFacade);
