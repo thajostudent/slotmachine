6f6274806868e9e78c96c3b142953cfe3778a252
Fredrik Olsson <fredriko.olsson@gmail.com>
2017-12-23 15:35:00 +0000

    added test for isValidUrl

--- __tests__/unit/repo.test.js
+++ __tests__/unit/repo.test.js
@@ -1,4 +1,7 @@
+/* global expect */
+
 const fs = require('fs');
+const {isValidUrl, getRepoName } = require('../../lib/helpers/repo')
 const XMLStringToObject = require('../../lib/tools').XMLStringToObject;
 
 const testXML = fs.readFileSync('lib/test/config.xml');
@@ -23,4 +26,17 @@ describe('XMLStringToObject', () => {
         expect(err).toBeInstanceOf(Error);
       });
   });
+});
+
+
+const correctUrl = 'https://github.com/0dv000/xx00xx-exam-1';
+const badUrl = '/0dv000/xx00xx-exam-1';
+
+describe('isValidUrl', () => {
+  it('returns true on correct URL', () => (
+    expect(isValidUrl(correctUrl)).toBeTruthy()
+  ));
+  it('return false on bad URL', () => (
+    expect(isValidUrl(badUrl)).toBeFalsy()
+  ));
 });
