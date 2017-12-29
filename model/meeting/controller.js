2cd00326c362155556d6b7e0a5dc934d6698901a
Fredrik Olsson <fredriko.olsson@gmail.com>
2017-12-29 10:35:01 +0000

    fixed some bugs in jenkins integration

--- model/meeting/controller.js
+++ model/meeting/controller.js
@@ -1,16 +1,17 @@
 const Controller = require('../../lib/controller');
 const meetingFacade = require('./facade');
+//const jenkins = require('jenkins')({ baseUrl: `http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@194.47.174.55:8080`, crumbIssuer: true, promisify: true });
 var jenkinsapi = require('jenkins-api');
 var jenkins = jenkinsapi.init(`http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@194.47.174.55:8080`);
+const { getJob } = require('../../lib/helpers/repo');
 
 class MeetingController extends Controller {
 
     apiTest(req, res, next) {
-        console.log(req.body);
-
-        //var testresult = req.body.url + 'testReport/api/json';
-        jenkins.test_result('xx00xx-exam-1', '45', function(err, data) {
-            if(err){console.log(err);}
+        //{ url: 'http://194.47.174.55:8080/job/co000ol-exam1/1/' }
+        var obj = getJob(req.body.url)
+        //console.log(obj.name + obj.number)
+        jenkins.test_result(obj.name, obj.number, function(err, data) {
             console.log(data);
         });
 
