4a13cbf1eb6291eb0cc937583bd714ac9cf69278
Glitch (open-sausage) <none>
2018-01-04 16:16:35 +0000

    💰👟 Checkpoint

--- model/exam-result/schema.js
+++ model/exam-result/schema.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 
 const examResultSchema = new Schema({
-  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
-  exams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: true }]
+  users: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  exams: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: true },
+  isPassed: {type: Boolean, required: true}
 });
 
 
