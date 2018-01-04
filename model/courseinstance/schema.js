83cbe4818a8d2572c9305cc633f784128d1fb6ac
Glitch (open-sausage) <none>
2018-01-04 16:07:34 +0000

    🐥😥 Checkpoint

--- model/courseinstance/schema.js
+++ model/courseinstance/schema.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema;
 
 
 const courseinstanceSchema = new Schema({
+  course: {type: mongoose.Schema.Types.ObjectId, ref: 'Course'},
   year: { type: Number, required: true },
   semester: { type: String, required: true},
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  exa
 });
 
 
