c12ec79caa85eeb301551c9c3f6e7422cdb4a8a1
Glitch (open-sausage) <none>
2018-01-04 17:34:37 +0000

    💡👩‍👩‍👦 Checkpoint model/meeting/controller.js:247335

--- model/meeting/controller.js
+++ model/meeting/controller.js
@@ -1,6 +1,7 @@
 const Controller = require('../../lib/controller');
 const meetingFacade = require('./facade');
-const userFacade = require('..//facade');
+const userFacade = require('../user/facade');
+const courseFacade = require('../course/facade');
 var Jenkinsapi = require('jenkins-api');
 var jenkinsapi = Jenkinsapi.init(`http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@194.47.174.62:8080`);
 const jenkins = require('jenkins')({ baseUrl: `http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@194.47.174.62:8080`, crumbIssuer: true, promisify: true });
@@ -37,6 +38,7 @@ class MeetingController extends Controller {
             jenkinsapi.test_result(jobObj.name, jobObj.number, function(err, data) {
                 if (err) { console.log("getJobDataError"); }
                 console.log(data);
+          
             });
         }
     }
