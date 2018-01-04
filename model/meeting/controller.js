7b5a734210c6f6ac7d3809f9cf50ec255d72c861
Glitch (open-sausage) <none>
2018-01-04 18:53:39 +0000

    🍈🏅 Checkpoint

--- model/meeting/controller.js
+++ model/meeting/controller.js
@@ -39,8 +39,8 @@ class MeetingController extends Controller {
              const jenkinsData = await jenkinsapi.test_result(jobObj.name, jobObj.number, async function(err, data) {
               if(err){console.log(err)} 
               if(data.failCount === 0){
-                console.log(jobObj.name)  
-                console.log(jobObj.name.split("-")[0])
+                
+                
                   const user = await userFacade.findOne({username:jobObj.name.split("-")[0]});
                   const course = await courseFacade.findOne({title:'0dv000'})                  
                   course.users.push(user);
@@ -53,9 +53,9 @@ class MeetingController extends Controller {
                         headers: {
                         Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
                         'Content-Type': 'application/x-www-form-urlencoded'
-                        },
-                          data: `user=${req.body.user_id}`
-                        });
+                        }});
+                        
+                        console.log(response);
                       } catch(e){
                         console.log(e);
                       }
