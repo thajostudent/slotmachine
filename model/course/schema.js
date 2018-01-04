16dab5c3745611dd33c55508e18340616d47f8b9
Glitch (open-sausage) <none>
2018-01-04 16:04:34 +0000

    🎣🎀 Checkpoint model/courseinstance/facade.js:247335

--- model/course/schema.js
+++ model/course/schema.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
   title: { type: String, required: true },
-  courseinstances: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meeting' }]
+  courseinstances: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Courseinstance' }]
 });
 
 
