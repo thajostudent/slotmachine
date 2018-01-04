0c7942893ea2ab91ce6c7d0fe19e4e7ee6d8b976
Glitch (open-sausage) <none>
2018-01-04 18:57:40 +0000

    🍭🍩 Checkpoint model/course/schema.js:247335

--- model/userexam/controller.js
+++
@@ -1,6 +0,0 @@
-const Controller = require('../../lib/controller');
-const userexamFacade = require('./facade');
-
-class UserexamController extends Controller {}
-
-module.exports = new UserexamController(userexamFacade);
--- model/userexam/facade.js
+++
@@ -1,6 +0,0 @@
-const Facade = require('../../lib/facade');
-const userexamSchema = require('./schema');
-
-class UserexamFacade extends Facade {}
-
-module.exports = new UserexamFacade('Userexam', userexamSchema);
