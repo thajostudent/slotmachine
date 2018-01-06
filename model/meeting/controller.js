4cc4b0a861776c9a2995271c2284f394d2e070c0
Fredrik Olsson <fredriko.olsson@gmail.com>
2018-01-06 09:13:25 +0000

    made some fixes to messaging back user on passed test, added message on to user on failed tests

--- model/meeting/controller.js
+++ model/meeting/controller.js
@@ -5,8 +5,8 @@ const courseFacade = require('../course/facade');
 var Jenkinsapi = require('jenkins-api');
 var jenkinsapi = Jenkinsapi.init(`http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@194.47.174.62:8080`);
 const jenkins = require('jenkins')({ baseUrl: `http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@194.47.174.62:8080`, crumbIssuer: true, promisify: true });
-const { getJob, isValidUrl, createJenkinsConfigFile } = require('../../lib/helpers/repo');
-const axios = require('axios')
+const { getJob, createJenkinsConfigFile } = require('../../lib/helpers/repo');
+const axios = require('axios');
 
 class MeetingController extends Controller {
 
@@ -20,21 +20,22 @@ class MeetingController extends Controller {
                 await jenkins.job.create(req.body.repository.full_name.replace("/", "_"), jenkinsConfigXML);
             }
             catch (e) {
-                console.log(e);
+                console.log(e.message);
             }
         }
         // Starting buildjob on webhook release event
         else if (req.body.action === "published" && req.body.release.target_commitish === "master") {
             console.log("Starting buildjob");
+            console.log("body:\n",req.body);
             try {
-                await jenkins.job.build(req.body.repository.name);
+                await jenkins.job.build(req.body.repository.full_name.replace("/", "_"));
             }
             catch (e) {
-                console.log(e);
+                console.log(e.message);
             }
 
         }
-        // 
+        // Receives tests results data and sends a message to the student if they passed
         else if (req.body.url) {
             console.log(req.body.url);
             const jobObj = getJob(req.body.url);
@@ -47,16 +48,15 @@ class MeetingController extends Controller {
                     const course = await courseFacade.findOne({ title: jobObj.org });
                     course.users.push(user);
                     await course.save();
-                    // Send something the user
-                    console.log(course.channelid);
+                    // SEND SOMETHING TO THE USER
                     // get user id from user name
-                    const memberId = await getSlackUserId(jobObj.name.split("-")[0])
-
-
-
-                    console.log("memberid", memberId);
-                    // post message to user
-                    postResultToUser(memberId, course.channelid)
+                    const memberId = await getSlackUserId(jobObj.name.split("-")[0]);
+                    // post message to user that they can book an exam
+                    postMessageToSlackUser(memberId, course.channelid, "You tests have gone trough and you are now able to book an exam");
+                } else {
+                    const course = await courseFacade.findOne({ title: jobObj.org });
+                    const memberId = await getSlackUserId(jobObj.name.split("-")[0]);
+                    postMessageToSlackUser(memberId, course.channelid, `${data.failCount} tests failed, no cookie for you!`);
                 }
             });
         }
@@ -78,6 +78,7 @@ const getSlackUserId = async(username) => {
 
         let memberId;
         for (let i = 0; i < response.data.members.length; i += 1) {
+            // In production it should be reponse.data.members[i].name === username
             if (response.data.members[i].profile.display_name === username) {
                 memberId = response.data.members[i].id;
                 break;
@@ -92,7 +93,7 @@ const getSlackUserId = async(username) => {
 
 };
 
-const postResultToUser = async(userid, channelid) => {
+const postMessageToSlackUser = async(userid, channelid, message) => {
 
     console.log("channelid", channelid);
     console.log("userid", userid);
@@ -104,7 +105,7 @@ const postResultToUser = async(userid, channelid) => {
                 Authorization: `Bearer ${process.env.SLACK_API_TOKEN}`,
                 'Content-Type': 'application/json'
             },
-            data: { user: userid, text: "You tests have gone trough and you are now able to book an exam", channel: channelid }
+            data: { user: userid, text: message, channel: channelid }
         });
     }
     catch (e) { console.log(e.message); }
