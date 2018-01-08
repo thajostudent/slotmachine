13449d2fde915168d61bd708e35c5313033fc973
Fredrik Olsson <fredriko.olsson@gmail.com>
2018-01-08 13:24:18 +0000

    clean-up in repo controller

--- model/repo/controller.js
+++ model/repo/controller.js
@@ -1,17 +1,18 @@
 const Controller = require('../../lib/controller');
 const repoFacade = require('./facade');
-const { getOrgAndRepo, isValidUrl } = require('../../lib/helpers/repo');
-const { setGitHook } = require('../../lib/gitApiUtils');
-
 const userFacade = require('../user/facade');
 const courseFacade = require('../course/facade');
-var Jenkinsapi = require('jenkins-api');
-var jenkinsapi = Jenkinsapi.init(`http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@${process.env.JENKINS_URL}`);
-const jenkins = require('jenkins')({ baseUrl: `http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@${process.env.JENKINS_URL}`, crumbIssuer: true, promisify: true });
-const { getJob, createJenkinsConfigFile, getTestCases, getSlackUserId, postMessageToSlackUser } = require('../../lib/helpers/repo');
-
-const getSemester = require('../../lib/tools').getSemester;
 const examFacade = require('../exam/facade');
+
+const Jenkinsapi = require('jenkins-api');
+const jenkinsapi = Jenkinsapi.init(`http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@${process.env.JENKINS_URL}`);
+const jenkins = require('jenkins')({ baseUrl: `http://${process.env.JENKINS_USERNAME}:${process.env.JENKINS_PASSWORD}@${process.env.JENKINS_URL}`, crumbIssuer: true, promisify: true });
+
+const { getJob, createJenkinsConfigFile, getTestCases, getSlackUserId, postMessageToSlackUser } = require('../../lib/helpers/repo');
+const { getOrgAndRepo, isValidUrl } = require('../../lib/helpers/repo');
+const { setGitHook } = require('../../lib/gitApiUtils');
+const { getSemester } = require('../../lib/tools');
+
 
 class RepoController extends Controller {
   async addRepo(req, res, next) {
@@ -38,10 +39,10 @@ class RepoController extends Controller {
     }
     catch (e) {
       console.log(e);
-      if(e.message.includes("already exists")){
+      if (e.message.includes("already exists")) {
         return res.send({
-        text: `${repoUrl} has already been added`
-      });
+          text: `${repoUrl} has already been added`
+        });
       }
       return res.send({
         text: `There was a problem adding ${repoUrl} sorry about that!`
@@ -50,74 +51,68 @@ class RepoController extends Controller {
   }
 
   async hooks(req, res, next) {
-        // Setting up jenkins job on webhook creation
-        if (req.body.hook) {
-            res.sendStatus(200);
-            console.log("Setting up job on jenkins");
-            try {
-                const jenkinsConfigXML = await createJenkinsConfigFile({ scmUrl: "https://github.com/" + req.body.repository.full_name });
+    // Setting up jenkins job on webhook creation
+    if (req.body.hook) {
+      res.sendStatus(200);
+      console.log("Setting up job on jenkins");
+      try {
+        const jenkinsConfigXML = await createJenkinsConfigFile({ scmUrl: "https://github.com/" + req.body.repository.full_name });
 
-                await jenkins.job.create(req.body.repository.full_name.replace("/", "_"), jenkinsConfigXML);
-            }
-            catch (e) {
-                console.log(e.message);
-            }
-        }
-        // Starting buildjob on webhook release event
-        else if (req.body.action === "published" && req.body.release.target_commitish === "master") {
-            console.log("Starting buildjob");
-            res.sendStatus(200);
-            try {
-                await jenkins.job.build(req.body.repository.full_name.replace("/", "_"));
-            }
-            catch (e) {
-                console.log(e.message);
-            }
-
-        }
-        // Receives tests results data and sends a message to the student if they passed
-        else if (req.body.url) {
-            const jobObj = getJob(req.body.url);
-            console.log(jobObj);
-            // Get the test results from the jenkins server
-            jenkinsapi.test_result(jobObj.org + "_" + jobObj.name, jobObj.number, async function(err, data) {
-                if (err) { console.log(err.message) }
-                if (data.failCount === 0) {
-                    try {
-                        const user = await userFacade.findOne({ username: jobObj.name.split("-")[0] });
-                        const course = await courseFacade.findOne({ title: jobObj.org });
-                        const exam = await examFacade.findOne({ course: jobObj.org + getSemester(), name: jobObj.exam });  
-                        await examFacade.update(
-                          { _id: exam._id },
-                          { $push: { results: user } }
-                        );
-                        //course.users.push(user);
-                        await course.save();
-                        //await exam.save();
-                        // SEND SOMETHING TO THE USER
-                        // get user id from user name
-                        const memberId = await getSlackUserId(jobObj.name.split("-")[0]);
-                        // post message to user that they can book an exam
-                        postMessageToSlackUser(memberId, course.channelid, `<@${memberId}>\n You tests have gone trough and you are now able to book an exam`);
-                    }
-                    catch (e) { console.log(e.message); }
-                }
-                else {
-                    try {
-                        const course = await courseFacade.findOne({ title: jobObj.org });
-                        const memberId = await getSlackUserId(jobObj.name.split("-")[0]);
-                        //return all failed testcases in string
-                        const fails = getTestCases(data);
-
-                        postMessageToSlackUser(memberId, course.channelid, `<@${memberId}>\n *${data.failCount} tests failed, no cookie for you!* \n` + ` *You need to fix, to get the cookie:*\n ${fails}`);
-                    }
-                    catch (e) {
-                        console.log(e.message);
-                    }
-                }
-            });
-        }
+        await jenkins.job.create(req.body.repository.full_name.replace("/", "_"), jenkinsConfigXML);
+      }
+      catch (e) {
+        console.log(e.message);
+      }
     }
+    // Starting buildjob on webhook release event
+    else if (req.body.action === "published" && req.body.release.target_commitish === "master") {
+      console.log("Starting buildjob");
+      res.sendStatus(200);
+      try {
+        await jenkins.job.build(req.body.repository.full_name.replace("/", "_"));
+      }
+      catch (e) {
+        console.log(e.message);
+      }
+
+    }
+    // Receives tests results data and sends a message to the student if they passed
+    else if (req.body.url) {
+      // Break up to jenkins job url into useable chunks
+      const jobObj = getJob(req.body.url);
+      // Get the test results from the jenkins server
+      jenkinsapi.test_result(jobObj.org + "_" + jobObj.name, jobObj.number, async function(err, data) {
+        if (err) { console.log(err.message) }
+        if (data.failCount === 0) {
+          try {
+            const user = await userFacade.findOne({ username: jobObj.user });
+            const course = await courseFacade.findOne({ title: jobObj.org });
+            const exam = await examFacade.findOne({ course: jobObj.org + getSemester(), name: jobObj.exam });
+            await examFacade.update({ _id: exam._id }, { $push: { results: user } });
+            
+            // get user id from user name
+            const memberId = await getSlackUserId(jobObj.user);
+            // post message to user that they can book an exam
+            postMessageToSlackUser(memberId, course.channelid, `<@${memberId}>\n You tests have gone trough and you are now able to book an exam`);
+          }
+          catch (e) { console.log(e.message); }
+        }
+        else {
+          try {
+            const course = await courseFacade.findOne({ title: jobObj.org });
+            const memberId = await getSlackUserId(jobObj.user);
+            //return all failed testcases in string
+            const fails = getTestCases(data);
+
+            postMessageToSlackUser(memberId, course.channelid, `<@${memberId}>\n *${data.failCount} tests failed, no cookie for you!* \n` + ` *Fix the following to be able to book an exam:*\n ${fails}`);
+          }
+          catch (e) {
+            console.log(e.message);
+          }
+        }
+      });
+    }
+  }
 }
 
 module.exports = new RepoController(repoFacade);
