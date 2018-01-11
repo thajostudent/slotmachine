1c39c5107ed8b75b0592a3fa75d7272403b0af42
Francis Menkes <francis.menkes@gmail.com>
2018-01-11 20:30:53 -0300

    Demo

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ const course = require('./model/course/router');
 const checkSlackToken = require('./lib/middleware/checkSlackToken');
 
 router.route('/').get((req, res) => {
-  res.json({ message: 'Welcome to slotmachine API 2.0!' });
+  res.json({ message: 'Welcome to the slotmachine API!' });
 });
 
 router.use('/exams', checkSlackToken, exam);
