15fdb67278254618369011ac9006da98b82bd763
wallstedt <fredrik.wallstedt@gmail.com>
2018-01-12 11:41:32 +0100

    Push test

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express    = require('express');
-const mongoose   = require('mongoose');
-const helmet     = require('helmet');
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const morgan     = require('morgan');
-const bluebird   = require('bluebird');
+const morgan = require('morgan');
+const bluebird = require('bluebird');
 
 const config = require('./config');
 const routes = require('./routes');
 
 require('dotenv').config();
 
-const app  = express();
+const app = express();
 
 mongoose.Promise = bluebird;
 mongoose.connect(config.mongo.url, { useMongoClient: true });
@@ -31,4 +31,5 @@ const server = app.listen(config.server.port, () => {
   console.log(`Magic happens on port ${config.server.port}`);
 });
 
+// pushy push
 module.exports = server;
